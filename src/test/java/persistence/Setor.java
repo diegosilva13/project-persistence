918ff359193f2d5efb45a1fc0ab82f1b2155e2e7
diegosilva13 <diegokingdog13@gmail.com>
2014-02-10 16:07:27 -0300

    #ppo

---
+++ src/test/java/persistence/Setor.java
@@ -0,0 +1,22 @@
+package persistence;
+
+public class Setor {
+	public Integer id;
+	public String nome;
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
