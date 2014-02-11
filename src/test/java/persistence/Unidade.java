1b7ee1155b06b28a6544ff389d1e8019d3fd6b63
diegosilva13 <diegokingdog13@gmail.com>
2014-02-11 08:10:47 -0300

    #PPO Classe Unidade.java

---
+++ src/test/java/persistence/Unidade.java
@@ -0,0 +1,22 @@
+package persistence;
+
+public class Unidade {
+	private Integer id;
+	private Integer numero;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+}
